b13991154a2ca9eb52c8e4bf95b9dcbbffb58352
Mark Harfouche <mark.harfouche@gmail.com>
2022-05-07 19:32:11 -0400

    enable zlib ng on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 setlocal EnableDelayedExpansion
-rmdir /s /q internal-complibs\lz4-1.9.3
-rmdir /s /q internal-complibs\zstd-1.5.2
+rmdir /s /q internal-complibs\lz4-*
+rmdir /s /q internal-complibs\zstd-*
 
 mkdir build
 if errorlevel 1 exit 1
@@ -22,7 +22,7 @@ cmake -G "NMake Makefiles" ^
       -DBLOSC_INSTALL=ON ^
       -DPREFER_EXTERNAL_LZ4:BOOL=ON ^
       -DPREFER_EXTERNAL_ZSTD:BOOL=ON ^
-      -DPREFER_EXTERNAL_ZLIB:BOOL=OFF ^
+      -DPREFER_EXTERNAL_ZLIB:BOOL=ON ^
       "%SRC_DIR%"
 if errorlevel 1 exit 1
 
